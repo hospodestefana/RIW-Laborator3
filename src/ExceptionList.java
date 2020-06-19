9697b1a1e0087f82ca2e913938d1b060a206f19b
hospo <stefana-alexandra.hospode@student.tuiasi.ro>
2020-06-19 12:35:07 +0300

    Initial commit

---
+++ src/ExceptionList.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class ExceptionList {
+    public static List<String> exceptions = Arrays.asList(
+            "IETF",
+            "RFC",
+            "AMS",
+            "ISOC",
+            "UK",
+            "Montreal",
+            "IAB",
+            "AQM"
+    );
+}
